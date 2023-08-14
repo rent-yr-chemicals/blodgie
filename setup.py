aaf9466446115a9d49d2478043dd60f7032db2b9
Ember McIntyre <ember.mcintyre@proton.me>
2023-08-13 19:08:38 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+def main():
+    setup(
+        name='blodgie',
+        version='0.0',
+        packages=('blodgie',),
+        install_requires=['ikea_api','tabulate'],
+    )
+
+if __name__ == '__main__':
+    main()
